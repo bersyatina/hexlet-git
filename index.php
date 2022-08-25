fbbd95ea5351ed69b6f4357e5f8bd853ba50306d
Apanasenko Andrey <aw_apanasenko@mail.ru>
2022-08-25 16:38:51 +0000

    add files

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-echo "Hello, Hexlet!";
+// Путь для локальной работы с проектом
+require_once  __DIR__ . '/../vendor/autoload.php';
+
+// Файл не включается на прямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
